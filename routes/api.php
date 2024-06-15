0e2cbc0acc6d072d89e8fbce5cf371b110287f57
daverydan <danny.w.avery@gmail.com>
2024-06-14 20:28:38 -0400

    pa make:controller Api/CategoryController + api route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
