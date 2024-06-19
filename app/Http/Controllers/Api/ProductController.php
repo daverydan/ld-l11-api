c9c885ed5f3b3110dd22569e501cab4fa0634476
daverydan <danny.w.avery@gmail.com>
2024-06-19 09:32:07 -0400

    +Product route, ctrl & resource

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class ProductController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        $products = Product::with('category')->get();
+
+        return ProductResource::collection($products);
+    }
+}
