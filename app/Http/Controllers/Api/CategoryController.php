a77c0dd84a63ab8fc3366889dc1349c2ceae6dd7
daverydan <danny.w.avery@gmail.com>
2024-06-19 10:34:41 -0400

    +desc to model mass assign & Update/Delete category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,29 +6,44 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function index(): JsonResource
     {
         return CategoryResource::collection(Category::all());
     }
 
-    public function show(Category $category)
+    public function show(Category $category): JsonResource
     {
         return new CategoryResource($category);
     }
 
-    public function store(StoreCategoryRequest $request)
+    public function store(StoreCategoryRequest $request): JsonResource
     {
         $category = Category::create($request->validated());
 
         return new CategoryResource($category);
     }
 
-    public function list()
+    public function list(): JsonResource
     {
         return CategoryResource::collection(Category::all());
+    }
+
+    public function update(Category $category, StoreCategoryRequest $request): JsonResource
+    {
+        $category->update($request->validated());
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category): Response
+    {
+        $category->delete();
+
+        return response()->noContent();
     }
 }
