15dde4a3c717e370e16d0d7f06b454540744d18f
daverydan <danny.w.avery@gmail.com>
2024-06-19 10:00:08 -0400

    +/use: StoreCategoryRequest

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -19,9 +20,9 @@ class CategoryController extends Controller
         return new CategoryResource($category);
     }
 
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        $category = Category::create($request->all());
+        $category = Category::create($request->validated());
 
         return new CategoryResource($category);
     }
